343fee98c40eeb2bf397061e86fe12171c8af69e
Sezil Akdoğan <111681902+SezilAkdogan@users.noreply.github.com>
2023-04-06 19:39:14 +0300

    Onboarding coding completed

---
+++ OnboardingGroup/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,76 @@
+//
+//  OnboardingViewController.swift
+//  TicketGO
+//
+//  Created by Sezil Akdoğan on 1.04.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    var slides: [OnboardingSlide] = []
+    
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextBtn.setTitle("Get Started", for: .normal)
+            } else {
+                nextBtn.setTitle("Continue", for: .normal
+                )
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slides.append(OnboardingSlide(title: "Welcome to TicketGO", description: "Thank you for choosing to travel with us. With our TicketGO, you can easily search for bus tickets and make reservations.", image:UIImage(named: "welcome")!))
+        slides.append(OnboardingSlide(title: "Easy Search", description: "Searching for TicketGO is very easy! Simply select the cities and dates you want to travel, and then view all the options with just a few taps.", image: UIImage(named: "location")!))
+        slides.append(OnboardingSlide(title: "TicketGO: Simplify Your Travels", description: "Start using TicketGO now!", image: UIImage(named: "start")!))
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextBtnClicked(_ sender: UIButton) {
+        
+        if currentPage == slides.count - 1 {
+            let controller = storyboard?.instantiateViewController(withIdentifier: "HomeNC") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle = .flipHorizontal
+            
+            present(controller, animated: true)
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+    }
+}
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width =  scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+        
+    }
+    
+    
+}
+
